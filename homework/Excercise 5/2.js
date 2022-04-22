4cd85740c7492a4f6df7923608959b7ac8f4e483
MitanEXE <59639861+MitanEXE@users.noreply.github.com>
2022-04-22 10:55:17 +0800

    Update 2.js

--- homework/Excercise 5/2.js
+++ homework/Excercise 5/2.js
@@ -1,6 +1,21 @@
-let a = 10; // input
-let b = a; // storing data
-for (let index = a; index > 1; index--) {
-    b *= index;
+// Basic way, without Functions
+// let a = 10; // input
+// let b = a; // storing data
+// for (let index = a; index > 1; index--) {
+//     b *= index;
+// }
+// console.log("%s! = %s",a,b);
+
+// Using Functions
+let a = 10;
+let b = a;
+function factorial(params){
+    b *= params;
+    if(params == 1){
+        console.log(b);
+    } else {
+        factorial(params-1);
+    }
 }
-console.log("%s! = %s",a,b);
+
+factorial(a-1);
